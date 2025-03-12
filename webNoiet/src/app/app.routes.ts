ab87b1e3b6c48c1c5a61ee98e0a13e135a79f80c
Grigs2 <otaviogrigolon@gmail.com>
2025-03-12 18:36:09 -0300

    Terceira Commit

---
+++ webNoiet/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { LoginComponent } from './login/login.component';
+export const routes: Routes = [
+
+    {path:"cadastro", component:CadastroComponent},
+    {path:"login", component:LoginComponent},
+    {path:"", component:CadastroComponent}
+];
